23cdda7784f5b64e25194f9c181c9208c61c7c53
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-04-19 21:53:14 +0530

    finshed assignment

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import {Button} from "antd"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+    </div>
+  )
+}
+
+export default App
